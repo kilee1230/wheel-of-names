5301b58e65cd6d348ee8fcfb1fc0b86d0e1ed513
Kin Fei Lee <kilee@seekasia.com>
2025-05-28 16:33:07 +0800

    feat: modify build cmd

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/wheel-of-names/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name?.endsWith(".wav")) {
+            return "sounds/[name].[ext]";
+          }
+          return "assets/[name].[ext]";
+        },
+      },
+    },
+  },
+  publicDir: "sounds", // Ensure sounds directory is copied to dist
 });
